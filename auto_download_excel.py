9ee8ae689bb9afa129f2944979434c993e4ed864
maruthirm91 <65456427+maruthirm91@users.noreply.github.com>
2021-07-13 23:52:32 +0530

    Update auto_download_excel.py

--- auto_download_excel.py
+++ auto_download_excel.py
@@ -9,13 +9,14 @@ id=df['ID']
 df_dict=dict(zip(id,link))
 print('list is converted to dictionary!!')
 
+#iterating over the web links from the excel read earlier and stored in df.
 for key, value in df_dict.items():
     print('sending HTTP requests for: ', key)
     r=requests.get(value)
-    if value.find('.'):
-        extn='.'+ value.rsplit('.',1)[1]
-        name='APU-'+str(key)+str(extn)
+    if value.find('.'):  #finding the '.' from the URL to locate the file extension
+        extn='.'+ value.rsplit('.',1)[1] #retrieving the word after '.'. Usually the file download URL's will have file extensions as last string
+        name='APU-'+str(key)+str(extn) # appending the Key value from dictionary to file extension and name it to file being downloaded
     with open(name,'wb') as f:
-        f.write(r.content)
+        f.write(r.content) # writing the content of response recieved from HTTP request.
 
 print('files downloaded')
